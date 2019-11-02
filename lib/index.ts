1f02f2f2f79680fb56fd6eb5d04c235644452af7
ufoo68 <mitarou.yuutarou@gmail.com>
2019-11-02 23:57:28 +0900

    [change]Use official sdk for Type Message

--- lib/index.ts
+++ lib/index.ts
@@ -1,19 +1,15 @@
-'use strict';
 // Message builder for LINE Messaging API
-
-export default interface ReplyMessage {
-    type: string,
-    text: string
-}
+'use strict';
+import { Message } from '@line/bot-sdk'
 
 /**
 * @Method: Return message object or Array[object].
 * @Param {string | string[]}
 * @Return {ReplyMessage | ReplyMessage[]}
 */
-export function buildReplyText(text: string): ReplyMessage;
-export function buildReplyText(text: string[]): ReplyMessage[];
-export function buildReplyText(text: string | string[]) : ReplyMessage | ReplyMessage[] {
+export function buildReplyText(text: string): Message;
+export function buildReplyText(text: string[]): Message[];
+export function buildReplyText(text: string | string[]) : Message | Message[] {
     return Array.isArray(text) ? text.map(t => 
     (
         {
@@ -32,6 +28,6 @@ export function buildReplyText(text: string | string[]) : ReplyMessage | ReplyMe
 * @Param {string[]}
 * @Return {ReplyMessage}
 */
-export function buildReplyRandomText(texts: string[]) : ReplyMessage {
-    return buildReplyText(texts[ Math.floor( Math.random() * texts.length )]) as ReplyMessage
+export function buildReplyRandomText(texts: string[]) : Message {
+    return buildReplyText(texts[ Math.floor( Math.random() * texts.length )]) as Message
 }
